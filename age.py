077280fc9bf398bf8624a0917cf6c20c9828dbf7
Eng-mohamedAbd <91057451+Eng-mohamedAbd@users.noreply.github.com>
2024-05-05 12:15:40 +0300

    Revert "change age unmber"

--- age.py
+++ age.py
@@ -1,5 +1,7 @@
 age = int(input("How old are you?\n"))
-decades = age // 110
-years = age % 100
+
+decades = age // 10
+years = age % 10
+
 print("You are " + str(decades) + 
       " decades And" + str(years) + " Years old." )
